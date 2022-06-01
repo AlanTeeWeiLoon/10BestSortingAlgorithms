da4a2c845cb4a818b9c812ecab8270a348046b24
Alan <alantee0302@gmail.com>
2022-06-01 23:24:26 +0800

    update

---
+++ insertion-sort/src/App.vue
@@ -0,0 +1,78 @@
+<template>
+<div>
+  <InsertionSort msg="Insertion Sort"/>
+  <div>
+      <button @click="randomNum">
+        Click to random generate 10 numbers
+      </button>
+    </div>
+    <div>
+    <table class="center">
+      <tr>
+        <td v-for="(num) in data.arrNum" v-bind:key="num" style="width:2em;font-size: x-large;font-weight: bold;">{{num}}</td>
+      </tr>
+    </table>
+    </div>
+    <button @click="insertionSort(data.arrNum)">
+        Click for Insertion Sort
+      </button>
+  </div>
+    
+</template>
+
+<script>
+import InsertionSort from './components/InsertionSort.vue'
+
+export default {
+  name: 'App',
+  components: {
+    InsertionSort
+  },
+  data(){
+		return {
+			data: {
+        arrNum: [1,2,3,4,5,6,7,8,9,10]
+      }
+    }
+  },
+  methods: {
+    randomNum(){
+      var arr = [];
+      while(arr.length < 10){
+          var r = Math.floor(Math.random() * 100) + 1;
+          if(arr.indexOf(r) === -1) arr.push(r);
+      }
+      this.data.arrNum=arr;
+    },
+
+    insertionSort(array){
+      for (let i = 1; i < array.length; i++) {
+    let currentValue = array[i]
+    let j
+    for (j = i - 1; j >= 0 && array[j] > currentValue; j--) {
+      array[j + 1] = array[j]
+    }
+    array[j + 1] = currentValue
+  }
+      this.data.arrNum = array
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.center {
+  margin-left: auto;
+  margin-right: auto;
+  margin-top:1em;
+  margin-bottom:1em;
+}
+</style>
---
+++ insertion-sort/src/components/InsertionSort.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    
+    <p>
+      Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time. 
+    </p>
+    <p>
+      It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort.
+    </p>
+    
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InsertionSort',
+  props: {
+    msg: String
+  },
+  
+
+  
+}
+
+
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
