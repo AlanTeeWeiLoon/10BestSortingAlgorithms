d8667f9f9fd2b8424f60c816967b996546413f31
Alan <alantee0302@gmail.com>
2022-06-06 19:13:23 +0800

    create heap sort

---
+++ heap-sort/src/components/HeapSort.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    
+    <p>
+      Shell sort is an optimized version of Insertion sort.
+    </p>
+    <p>
+      It differs from insertion sort in that it prioritizes elements that are farther apart. Shell sorting is also called shrinking incremental sorting.
+    </p>
+    
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeapSort',
+  props: {
+    msg: String
+  },
+  
+
+  
+}
+
+
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
