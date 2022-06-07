85186a515ea4226452ffe6e42d68bd18cdd6806c
Alan <alantee0302@gmail.com>
2022-06-08 00:03:03 +0800

    update counting sort

--- counting-sort/src/App.vue
+++ counting-sort/src/App.vue
@@ -1,17 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <CountingSort msg="Counting Sort" />
+    <div>
+      <button @click="randomNum">Click to random generate 10 numbers</button>
+    </div>
+    <div>
+      <table class="center">
+        <tr>
+          <td
+            v-for="num in data.arrNum"
+            v-bind:key="num"
+            style="width: 2em; font-size: x-large; font-weight: bold"
+          >
+            {{ num }}
+          </td>
+        </tr>
+      </table>
+    </div>
+    <button @click="countingSort(data.arrNum)">Click for Counting Sort</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountingSort from "./components/CountingSort.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    CountingSort,
+  },
+  data() {
+    return {
+      data: {
+        arrNum: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      },
+    };
+  },
+  methods: {
+    randomNum() {
+      var arr = [];
+      while (arr.length < 10) {
+        var r = Math.floor(Math.random() * 100) + 1;
+        if (arr.indexOf(r) === -1) arr.push(r);
+      }
+      this.data.arrNum = arr;
+    },
+
+    countingSort(array) {
+      let n = array.length;
+
+      //Start with a really large gap, and then reduce the gap until there isn't any
+      //With this, the gap starts as half of the array length, and then half of that every time
+      for (let gap = Math.floor(n / 2); gap > 0; gap = Math.floor(gap / 2)) {
+        //Do a insertion sort for each of the section the gap ends up dividing
+        for (let i = gap; i < n; i += 1) {
+          //We store the current varible
+          let temp = array[i];
+
+          //This is the insection sort to sort the section into order
+          let j;
+          for (j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+            array[j] = array[j - gap];
+          }
+
+          array[j] = temp;
+        }
+      }
+      this.data.arrNum = array;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +80,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.center {
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 1em;
+  margin-bottom: 1em;
 }
 </style>
---
+++ counting-sort/src/components/CountingSort.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    
+    <p>
+      Counting sort is an optimized version of Insertion sort.
+    </p>
+    <p>
+      It differs from insertion sort in that it prioritizes elements that are farther apart. Shell sorting is also called shrinking incremental sorting.
+    </p>
+    
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CountingSort',
+  props: {
+    msg: String
+  },
+  
+
+  
+}
+
+
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
